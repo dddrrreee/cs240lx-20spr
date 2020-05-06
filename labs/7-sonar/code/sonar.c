984a805c657d6a87de537a7d7aceddc50fc727dc
engler <ddd.rrr.eee@gmail.com>
2020-05-05 19:48:50 -0700

    minor

--- labs/7-sonar/code/sonar.c
+++ labs/7-sonar/code/sonar.c
@@ -2,9 +2,18 @@
 #include "rpi.h"
 #include "hc-sr04.h"
 
+// use this timeout(in usec) so everyone is consistent.
+unsigned timeout = 55000;
+
+#ifdef RPI_UNIX
+
+#   include "fake-pi.h"
+    // you can define a timer_get_usec here to be fancier
+    // unsigned timer_get_usec(void) { }
+
+#endif
+
 void notmain(void) {
-    // use this timeout(in usec) so everyone is consistent.
-    unsigned timeout = 55000;
 
     uart_init();
 
@@ -15,7 +24,7 @@ void notmain(void) {
     for(int dist, i = 0; i < 10; i++) {
         // read until no timeout.
         while((dist = hc_sr04_get_distance(&h, timeout)) < 0)
-            ;
+            printk("timeout!\n");
         printk("distance = %d inches\n", dist);
         // wait a second
         delay_ms(1000);
