d8c64b99cede2f826253c213aa42ae07b030e1ce
engler <ddd.rrr.eee@gmail.com>
2020-05-21 16:17:45 -0700

    minor

--- libpi/include/rpi-internal.h
+++ libpi/include/rpi-internal.h
@@ -5,4 +5,17 @@
 
 void (panic)(const char *file, int lineno, const char *msg);
 
+
+// symbols defined in memmap
+extern char 
+            __code_start__, // code.
+            __code_end__,
+            __bss_start__,  // 0 initialized data
+            __bss_end__,
+            __data_start__, // global r/w data
+            __data_end__,   
+            __prog_end__,   // entire end of program: code+data.
+            __heap_start__; // where the heap can start 
+                            // (right now = __prog_end__)
+
 #endif
