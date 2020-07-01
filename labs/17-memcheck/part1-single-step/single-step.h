afb2f36be64653577c84ecdf10095af7e34ac009
engler <ddd.rrr.eee@gmail.com>
2020-06-30 18:33:16 -0700

    minor

--- labs/17-memcheck/part1-single-step/single-step.h
+++ labs/17-memcheck/part1-single-step/single-step.h
@@ -1,10 +1,24 @@
 #ifndef __SINGLE_STEP_H__
 #define __SINGLE_STEP_H__
+#include "rpi.h"
+#include "cp14-debug.h"
+#include "bit-support.h"
 
-// initialize single step mode.
+// initialize single step mode: 1 on success.
 int single_step_init(void);
 
 // run a single routine <user_fn> in single step mode using stack <sp>
 int single_step_run(int user_fn(void), uint32_t sp);
+
+// assembly routine: run <fn> at user level with stack <sp>
+//    XXX: visible here just so we can use it for testing.
+int user_mode_run_fn(int (*fn)(void), uint32_t sp);
+
+/*************************************************************
+ * support code for tests.
+ */
+static inline int mode_eq(uint32_t mode) { return mode_get(cpsr_get()) == mode; }
+static inline int mode_is_super(void) { return mode_eq(SUPER_MODE); }
+static inline int mode_is_user(void) { return mode_eq(USER_MODE); }
 
 #endif
---
+++ labs/17-memcheck/part1-single-step/tests/part1-test1.c
@@ -0,0 +1,17 @@
+// engler: trivial check that you can switch to user mode.
+#include "memcheck.h"
+
+static int user_fn(void) {
+    trace("user mode = %b, current mode=%b\n", USER_MODE, mode_get(cpsr_get()));
+    assert(mode_is_user());
+    for(int i = 0; i < 10; i++) 
+        trace("hello %d\n", i);
+    clean_reboot();
+    return 4;
+}
+
+void notmain() {
+    assert(mode_is_super());
+    trace("about to try running at user level\n");
+    int x = user_mode_run_fn(user_fn, STACK_ADDR2);
+}
