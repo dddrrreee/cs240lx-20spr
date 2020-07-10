5081109ae6ad808fcd1e0aa4256e79ed9e35473a
engler <ddd.rrr.eee@gmail.com>
2020-07-09 19:10:49 -0700

    minor

--- labs/18-memcheck-impl/code/tests/part3-test0.c
+++ labs/18-memcheck-impl/code/tests/part3-test0.c
@@ -1,7 +1,11 @@
-// engler: make sure we can trap/resume multiple times.
+// engler: check that allocation works with shadow memory: allocate, do a bunch
+// of writes and read the result.
 #include "memcheck.h"
+#include "cpsr-util.h"
 
 static void test_resume(int *addr, uint32_t val) {
+    memcheck_alloc(4);
+
     trace("expect 'domain section fault' for addr %p, at pc %p\n", addr, put32);
     // should get a fault here.
     put32(addr, val);
@@ -11,22 +15,26 @@ static void test_resume(int *addr, uint32_t val) {
     trace("correct: %p == %d\n", addr, val);
 }
 
+unsigned expected = 45;
+
 int notmain_client() {
-    printk("in notmain_client: going to kmalloc!\n");
-    int *x = kmalloc(4);
-    printk("going to run test_resume\n");
-    for(int i = 0; i < 10; i++) 
-        test_resume(x, i);
-    return 0x12345678;
+    volatile int *x = memcheck_alloc(4);
+
+    for(int i = 0; i < 10; i++)
+        *x += i;
+    printk("*x = %d\n", *x);
+    assert(get32(x) == expected);
+    return *x;
 }
 
 void notmain() {
     assert(!mmu_is_enabled());
 
     int x = memcheck_fn(notmain_client);
-    assert(x == 0x12345678);
-
     assert(!mmu_is_enabled());
+
+    assert(x == expected);
+
 
     trace_clean_exit("success!!\n");
 }
--- labs/18-memcheck-impl/code/tests/part3-test1.c
+++ labs/18-memcheck-impl/code/tests/part3-test1.c
@@ -1,13 +1,10 @@
 // engler: make sure you detect use after free
-//
-// XXX: need to add checks that we caught the right exception.
 #include "memcheck.h"
-#include "cpsr-util.h"
 
 int notmain_client() {
     int *x = memcheck_alloc(4);
-    memcheck_free(x);
-    *x = 1;
+    memcheck_free(x); 
+    *x = 1; // should get an error here.
     return 0;
 }
 
