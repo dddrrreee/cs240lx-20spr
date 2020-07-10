9790bd439ff828944c0ac1b19782ce84dc96731b
engler <ddd.rrr.eee@gmail.com>
2020-07-09 21:46:18 -0700

    minor

--- labs/18-memcheck-impl/code/tests/part3-test0.c
+++ labs/18-memcheck-impl/code/tests/part3-test0.c
@@ -3,22 +3,11 @@
 #include "memcheck.h"
 #include "cpsr-util.h"
 
-static void test_resume(int *addr, uint32_t val) {
-    memcheck_alloc(4);
-
-    trace("expect 'domain section fault' for addr %p, at pc %p\n", addr, put32);
-    // should get a fault here.
-    put32(addr, val);
-    trace("checking that %p == %d\n", addr, val);
-
-    assert(get32(addr) == val);
-    trace("correct: %p == %d\n", addr, val);
-}
-
 unsigned expected = 45;
 
 int notmain_client() {
     volatile int *x = memcheck_alloc(4);
+    *x = 0;
 
     for(int i = 0; i < 10; i++)
         *x += i;
