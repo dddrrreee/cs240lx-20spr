b35a19f626eff079f50aa4cca16ab7621c9fe994
engler <ddd.rrr.eee@gmail.com>
2020-08-13 19:43:02 -0700

    added

--- libpi/staff-src/cstart.c
+++ libpi/staff-src/cstart.c
@@ -10,15 +10,44 @@ static int internal_sw_putchar(int c) {
 }
 #endif
 
+#if 0
+static void _cstart_reloc() {
+#if 0
+    extern char __data_start__, __data_end__, __data_lma__, __code_end__;
+    char *data = &__data_start__;
+    char *data_end = &__data_end__;
+    char *code_end = &__code_end__;
+    if(data != code_end)
+        memcpy(data, &__data_lma__, data_end-data);
+#else
+    return;
+    extern char __data_start__, __data_end__,__code_end__;
+    char *data = &__data_start__;
+    char *data_end = &__data_end__;
+    char *code_end = &__code_end__;
+    if(data != code_end)
+        memcpy(data, code_end, data_end-data);
+
+#endif
+}
+#endif
+
 void _cstart() {
     extern int __bss_start__, __bss_end__;
 	void notmain();
+
+    custom_loader();
 
     int* bss = &__bss_start__;
     int* bss_end = &__bss_end__;
  
     while( bss < bss_end )
         *bss++ = 0;
+
+#if 0
+    _cstart_reloc();
+
+#endif
 
 #   ifdef RPI_FP_ENABLED
 #   include "rpi-fp.h"
@@ -42,4 +71,5 @@ void _cstart() {
     notmain(); 
 	clean_reboot();
 }
+
 
